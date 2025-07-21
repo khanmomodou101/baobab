ffef6196acb69cb679f80908803117b82c0e5aa5
khanreformiqo <momdou.khan@reformiqo.com>
2025-07-21 19:19:25 +0000

    update

--- baobab/api.py
+++ baobab/api.py
@@ -80,6 +80,8 @@ def remove_duplicate_sales_invoice(inv, method=None):
                 unique_payments.append(payment)
         
         doc.payments = unique_payments
+        doc.save()
+        frappe.db.commit()
         frappe.log_error(f"Removed duplicate payments from sales invoice {unique_payments}")
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Error removing duplicate payments from sales invoice")
